3a9108dede09638546397769089102dad9f2b726
Matt Bimpson <m.bimpson@gmail.com>
2020-01-23 10:06:28 -0500

    initial commit

---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+namespace todo_blazor.Models
+{
+  public class Todo
+  {
+    public int Id { get; set; }
+
+    public string Description { get; set; }
+
+    public bool Completed { get; set; }
+  }
+}
